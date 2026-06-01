49989b40f7dd9f28749bbce315e259257e747637
EC2 Default User <ec2-user@ip-172-31-46-186.ap-northeast-2.compute.internal>
2026-06-01 07:21:16 +0000

    new

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App(props) {
             <CloudIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-            EKS DEMO Blog
+            EEKKSSKKEE
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
